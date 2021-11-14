74dca42857d98375700cc9c36f64c2a7917ed46b
Janik Brüll <jbruell@aol.com>
2021-11-14 16:23:33 +0100

    Initial commit
    Setup Spring application

---
+++ src/main/java/com/jbruell/donutpriorityqueue/DonutPriorityQueueApplication.java
@@ -0,0 +1,13 @@
+package com.jbruell.donutpriorityqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutPriorityQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonutPriorityQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jbruell/donutpriorityqueue/DonutPriorityQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbruell.donutpriorityqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutPriorityQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
